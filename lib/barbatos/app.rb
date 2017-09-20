eea8c56e6c669f8b75969b9cf7f4e71fceaee8c0
okazu-dm <uhavetwocows@gmail.com>
2017-09-20 21:44:36 +0900

    Fix

--- lib/barbatos/app.rb
+++ lib/barbatos/app.rb
@@ -15,6 +15,17 @@ module Barbatos
     end
 
     class << self
+      def process(env)
+        @env = env
+        @context = Barbatos::Context.new(env)
+        path = @context.request.path
+        request_method = @context.request.request_method
+        action = @router[build_route(request_method, path)]
+        return @context.not_found.response if action.nil?
+        action.call(@context)
+        @context.response
+      end
+
       def render_text(text, status: 200, header: {})
         @context.response = Rack::Response.new(text.to_s, status, header)
       end
@@ -30,17 +41,6 @@ module Barbatos
       def put(path, &block)     route 'GET', path, &block end
       def delete(path, &block)  route 'GET', path, &block end
       # rubocop:enable all
-
-      def process(env)
-        @env = env
-        @context = Barbatos::Context.new(env)
-        path = @context.request.path
-        request_method = @context.request.request_method
-        action = @router[build_route(request_method, path)]
-        return res_404 if action.nil?
-        action.call(@context)
-        @context.response
-      end
 
       def route(request_method, path, &block)
         @router ||= {}
